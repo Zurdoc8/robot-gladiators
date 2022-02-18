f1b1dad4d71d7c3d5f7468b1f67521f37f44ebb9
Andres <garelan@gmail.com>
2022-02-18 03:48:57 -0600

    no fights

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,21 +3,14 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length);
-  for(var i = 0; i < 3; i++) {
-    console.log("apple");
-  }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-    // Alert players that they are starting the round
+var fight = function(_enemyName) {
+  // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 }
     var promptFight = window.prompt ("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' OR 'SKIP' to choose.");
@@ -69,4 +62,6 @@ var fight = function() {
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
-//fight()
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
