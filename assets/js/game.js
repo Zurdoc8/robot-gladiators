36affe28b66ae383b26034cfa7402be9066997be
Andres <garelan@gmail.com>
2022-02-18 23:45:10 -0600

    game over message

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,7 @@ var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 24;
 
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -68,19 +68,23 @@ var fight = function(enemyName) {
 
 for(var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
-      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
   
-      // pick new enemy to fight based on the index of the enemyNames array
-      var pickedEnemyName = enemyNames[i];
-  
-      // reset enemyHealth before starting new fight
-      enemyHealth = 50;
-  
-      // use debugger to pause script from running and check what's going on at that moment in the code
-      // debugger;
-  
-      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-      fight(pickedEnemyName);
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+    
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+    
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+    
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      } 
+  else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
     }
+    fight(pickedEnemyName);
   }
